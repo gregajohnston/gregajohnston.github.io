961548946b1af01365e3d8ff6dce1e14fb31254c
Greg Johnston <greg.a.johnston@gmail.com>
2016-06-26 05:10:30 -0400

    More icon work

--- js/navlock.js
+++ js/navlock.js
@@ -1,10 +1,5 @@
-$(window).scroll(function(){
-  // $('#nav_wrapper').toggleClass('scrolling', $(window).scrollTop() > $('#header').offset().top);
-
-  //can be rewritten long form as:
-  var scrollPosition, headerOffset, isScrolling;
-  scrollPosition = $(window).scrollTop();
-  headerOffset = $('#header').offset().top;
-  isScrolling = scrollPosition > headerOffset;
-  $('#nav_wrapper').toggleClass('scrolling', isScrolling);
+$(window).scroll(function() {
+    var currentPosition = $(window).scrollTop();
+    var startFixedNav = $('#body_wrapper').offset().top;
+    $('#nav_wrapper').toggleClass('scroll-lock', currentPosition > startFixedNav);
 });
